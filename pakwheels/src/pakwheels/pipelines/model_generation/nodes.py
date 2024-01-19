9b9d11098e6eaa8c6e076478c308ce5501e023e8
lef8 <alan.sipiorski14@gmail.com>
2024-01-19 18:44:49 +0100

    Change pickle output file extension

--- pakwheels/src/pakwheels/pipelines/model_generation/nodes.py
+++ pakwheels/src/pakwheels/pipelines/model_generation/nodes.py
@@ -2,16 +2,19 @@ import pandas as pd
 from sklearn.preprocessing import RobustScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-import pickle
+import joblib
 
-def generate_pickle(df: pd.DataFrame):
+
+def generate_model(df: pd.DataFrame):
     X = df.drop(columns="price", axis=1)
     y = df["price"]
     scalar = RobustScaler()
     X_scaled = scalar.fit_transform(X)
-    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=45)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X_scaled, y, test_size=0.2, random_state=45
+    )
     model = LinearRegression()
     model.fit(X_train, y_train)
-    pkl_model = pickle.dumps(model)
+    joblib.dump(model, "data/07_model_output/linear_regression_model.joblib")
 
-    return(pkl_model)
+    return model
--- pakwheels/src/pakwheels/pipelines/model_generation/pipeline.py
+++ pakwheels/src/pakwheels/pipelines/model_generation/pipeline.py
@@ -4,15 +4,17 @@ generated using Kedro 0.18.14
 """
 
 from kedro.pipeline import Pipeline, pipeline, node
-from .nodes import generate_pickle
+from .nodes import generate_model
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([
-        node(
-            func=generate_pickle,
-            inputs=["cleaned_data"],
-            outputs="generated_model",
-            name="generate_model_node"
-        )
-    ])
+    return pipeline(
+        [
+            node(
+                func=generate_model,
+                inputs=["cleaned_data"],
+                outputs="generated_model",
+                name="generate_model_node",
+            )
+        ]
+    )
