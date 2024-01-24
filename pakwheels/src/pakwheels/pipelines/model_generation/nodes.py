99f95d5452b8b3f262d0a3d2b06ecdbf5fd493df
lef8 <alan.sipiorski14@gmail.com>
2024-01-25 00:02:27 +0100

    Generate joblib file

--- pakwheels/src/pakwheels/pipelines/model_generation/nodes.py
+++ pakwheels/src/pakwheels/pipelines/model_generation/nodes.py
@@ -1,20 +1,8 @@
-import pandas as pd
-from sklearn.preprocessing import RobustScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
 import joblib
 
 
-def generate_model(df: pd.DataFrame):
-    X = df.drop(columns="price", axis=1)
-    y = df["price"]
-    scalar = RobustScaler()
-    X_scaled = scalar.fit_transform(X)
-    X_train, X_test, y_train, y_test = train_test_split(
-        X_scaled, y, test_size=0.2, random_state=45
-    )
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-    joblib.dump(model, "data/07_model_output/linear_regression_model.joblib")
+def generate_model(pipeline: Pipeline):
+    joblib.dump(pipeline, "data/07_model_output/prediction_ready_model.joblib")
 
-    return model
+    return pipeline
